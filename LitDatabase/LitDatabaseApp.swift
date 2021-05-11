9d12179efe1b56203e69923c044d74656cd334b1
droski <sandrodin28@gmail.com>
2021-05-10 23:16:13 -0500

    Initial Commit

---
+++ LitDatabase/LitDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitDatabaseApp.swift
+//  LitDatabase
+//
+//  Created by Sandro Dinklang on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LitDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
